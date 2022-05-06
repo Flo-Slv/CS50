a088079dfbc06c148dc6a5ab7092c1df6ef708c4
Flo Slv <11678456+Flo-Slv@users.noreply.github.com>
2022-05-06 13:11:44 +0200

    Create 13.sql

---
+++ Week 7/movies/13.sql
@@ -0,0 +1,1 @@
+SELECT name FROM people JOIN stars ON stars.person_id=people.id JOIN movies ON movies.id=stars.movie_id WHERE movies.id IN(SELECT movies.id FROM movies JOIN stars ON stars.movie_id=movies.id JOIN people ON people.id=stars.person_id WHERE people.name='Kevin Bacon' and people.birth=1958) and people.name != 'Kevin Bacon';
