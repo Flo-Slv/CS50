17fba6c7a81298ed7ef48683701db9e7ac5e4dd1
Flo Slv <11678456+Flo-Slv@users.noreply.github.com>
2022-05-06 13:09:49 +0200

    Create 9.sql

---
+++ Week 7/movies/9.sql
@@ -0,0 +1,1 @@
+SELECT DISTINCT(name) FROM people JOIN stars ON stars.person_id=people.id JOIN movies ON movies.id=stars.movie_id JOIN ratings ON ratings.movie_id=movies.id WHERE movies.year=2004 ORDER BY people.birth;
