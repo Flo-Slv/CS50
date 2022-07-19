91d675028f4d2f9c2bd7464fd0c96b5a6fb64027
Flo Slv <florent3969@hotmail.fr>
2022-07-19 19:16:07 +0200

    Add createComment mutation

--- FinalProject/backend/graphql/mutations/comment/createCommentMutation.js
+++ FinalProject/backend/graphql/mutations/comment/createCommentMutation.js
@@ -1,4 +1,33 @@
-const createCommentMutation = async (body, context) => {
+import { UserInputError } from 'apollo-server';
+
+import Post from '../../../models/Post.js';
+
+import checkAuth from '../../../utils/check-auth.js';
+
+const createCommentMutation = async (body, postId, context) => {
+	const { username } = checkAuth(context);
+
+	// If try to post empty comment.
+	if (body.trim() === '')
+		throw new UserInputError('Empty comment', {
+			errors: {
+				body: 'Comment must not be empty !'
+			}
+		});
+
+	const post = await Post.findById(postId);
+
+	if (post) {
+		// Add comment to the top.
+		post.comments.unshift({
+			body,
+			username,
+			createdAt: new Date().toLocaleString()
+		});
+
+		await post.save();
+		return post;
+	} else throw new UserInputError('Post not found');
 };
 
 export default createCommentMutation;
--- FinalProject/backend/graphql/resolvers/comments.js
+++ FinalProject/backend/graphql/resolvers/comments.js
@@ -3,8 +3,8 @@ import deleteCommentMutation from '../mutations/comment/deleteCommentMutation.js
 
 const commentResolver = {
 	Mutation: {
-		createComment: async (_, { body }, context) => {
-			return await createCommentMutation(body);
+		createComment: async (_, { body, postId }, context) => {
+			return await createCommentMutation(body, postId, context);
 		},
 		deleteComment: async (_, { body }, context) => {
 			return await deleteCommentMutation(body);
