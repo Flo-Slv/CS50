c987ca49c05b45e837516719852a272f4741371d
Flo Slv <florent3969@hotmail.fr>
2022-07-20 22:35:42 +0200

    refactor:  remove ?

--- FinalProject/index.js
+++ FinalProject/index.js
@@ -6,7 +6,7 @@ import { MONGODB } from './backend/config.js';
 import typeDefs from './backend/graphql/typeDefs.js';
 import resolvers from './backend/graphql/resolvers/index.js';
 
-// tutorial: 1:46:26 ???
+// tutorial: 1:46:26 ??
 
 const server = new ApolloServer({
 	typeDefs,
